f889c354c9482a4fa3d45871e5f5cffee8f7ea2d
Thia <thiafelicien3@gmail.com>
2024-06-28 13:02:54 +0200

    Oublie de index.js , renvoie de la MAJ

---
+++ index.js
@@ -0,0 +1,37 @@
+const app = require('express')();
+const cors = require('cors');
+const body_parser  = require('body-parser');
+
+ //\\  //  ------------ Independance ---------------- //\\
+//  \\//  -------------  en haut ------------------- //  \\ 
+
+
+const port  = 7507;
+
+
+
+/* ----------------------------------------------------------------------- */
+app
+    .use(body_parser.json())
+    .use(cors())
+/*-----------------------------------------------------------------------*/
+
+
+// ===> Acceuill here !!!!
+
+    require('./Accueil/accueil_node')(app) // http://localhost:7507/  <= click on this link 
+
+/* ------------------------------------------------------------------ */
+
+
+
+                                              //    'http://localhost:7507/ ++ >   
+require('./!@#--D!Dos/---Here__IS/nothing!/Az3/routes/connexion')(app)           //     '/TySyNday/connexion/'
+require('./!@#--D!Dos/---Here__IS/nothing!/Az3/routes/inscription')(app)        //      '/TySyNday/inscription/'
+require('./!@#--D!Dos/---Here__IS/nothing!/Az3/routes/modification')(app)      //       '/TySyNday/modification/{{id}}'   
+require('./!@#--D!Dos/---Here__IS/nothing!/Az3/routes/pub_For_acceuil')(app)  //        '/TySyNday/pubAcceuil/'
+require('./!@#--D!Dos/---Here__IS/nothing!/Az3/routes/message')(app)         //         '/TySyNday/message/(Envoie || Recevoir)'
+require('./!@#--D!Dos/---Here__IS/nothing!/Az3/routes/DonneesPersonne')(app)//          ' --- admin only ---- '
+
+
+app.listen(port,() => console.log(`port : => ${port}  \n http://localhost:${port}/`))
